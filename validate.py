08683bc623b2d962e7cd1c09213ad55fdb646b4c
Giorgos Stamatelatos <gstamatelat@gmail.com>
2021-04-21 22:47:10 +0300

    Update the validation script

--- validate.py
+++ validate.py
@@ -7,5 +7,5 @@ with open('celebrations.json', encoding='utf-8') as json_file:
 for entry in json_celebrations:
     if "names" in entry["celebrations"]:
         for name in entry["celebrations"]["names"]:
-            assert re.search(r'\s', name) is None
-            assert re.search(r'[^Α-Ω]', name) is None
+            assert re.search(r'\s', name) is None, "{} failed the whitespace check".format(name)
+            assert re.search(r'[^Α-Ω]', name) is None, "{} failed the greek letters check".format(name)
